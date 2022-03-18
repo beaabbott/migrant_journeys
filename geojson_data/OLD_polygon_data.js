ce375ea78beba71e0e7571c1472f3e6b8a6fe599
bea <92592012+beaabbott@users.noreply.github.com>
2022-03-18 19:59:36 -0400

    start project

---
+++ geojson_data/OLD_polygon_data.js
@@ -0,0 +1,151 @@
+var polygonData = [{
+        "type": "FeatureCollection",
+        "name": "polygon1",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "id": null
+            },
+            "geometry": {
+                "polyName": "polygon1",
+                "type": "MultiLineString",
+                "coordinates": [
+                    [
+                        [26.8985799381, 37.8195740727],
+                        [26.8996591502, 37.2083704672],
+                        [27.3335024227, 37.2092299917],
+                        [27.3345816349, 37.8195740727],
+                        [26.8996591502, 37.8187215489]
+                    ]
+                ]
+            }
+        }]
+    },
+
+    {
+        "type": "FeatureCollection",
+        "name": "polygon2",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "id": null
+            },
+            "geometry": {
+                "polyName": "polygon2",
+                "type": "MultiLineString",
+                "coordinates": [
+                    [
+                        [26.9073896246, 37.6528744373],
+                        [26.9461244626, 37.8384149299],
+                        [27.3560912522, 37.7833894275],
+                        [27.3162652423, 37.5957723637],
+                        [26.9080294925, 37.6527851099]
+                    ]
+                ]
+            }
+        }]
+    },
+    {
+        "type": "FeatureCollection",
+        "name": "polygon3",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "id": null
+            },
+            "geometry": {
+                "polyName": "polygon3",
+                "type": "MultiLineString",
+                "coordinates": [
+                    [
+                        [26.8395140946, 38.1576010865],
+                        [27.2714073353, 38.1576010865],
+                        [27.2736985461, 37.5851652236],
+                        [26.8406597, 37.5842573858],
+                        [26.8418053054, 38.1567002751]
+                    ]
+                ]
+            }
+        }]
+    },
+
+    {
+        "type": "FeatureCollection",
+        "name": "poly3_data",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "id": null
+            },
+            "geometry": {
+                "polyName": "polygon4",
+                "type": "MultiLineString",
+                "coordinates": [
+                    [
+                        [26.6015587872, 38.454543151],
+                        [28.2542140372, 38.459658374],
+                        [28.2607462714, 36.168890689],
+                        [26.6015587872, 36.158343284],
+                        [26.6146232556, 38.454543151]
+                    ]
+                ]
+            }
+        }]
+    },
+
+    {
+        "type": "FeatureCollection",
+        "name": "polygon5",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "id": null
+            },
+            "geometry": {
+                "polyName": "polygon5",
+                "type": "MultiLineString",
+                "coordinates": [
+                    [
+                        [26.0074427322, 38.5383064647],
+                        [26.4086631899, 38.5398008841],
+                        [26.4067526163, 38.0088392757],
+                        [26.0055321586, 38.0103446313],
+                        [26.0055321586, 38.5398008841]
+                    ]
+                ]
+            }
+        }]
+    }, 
+    
+
+]
---
+++ geojson_data/poly_data2.js
@@ -0,0 +1,129 @@
+var polygonData2 = [{
+        "type": "FeatureCollection",
+        "name": "poly1_polydata",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "id": null
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [26.9019348693, 37.8168020412],
+                            [27.330946344, 37.8168020412],
+                            [27.3291284988, 37.2372555316],
+                            [26.9092062502, 37.2343609697],
+                            [26.9019348693, 37.8124937573],
+                            [26.9019348693, 37.8168020412]
+                        ]
+                    ]
+                ]
+            }
+        }]
+    },
+    {
+        "type": "FeatureCollection",
+        "name": "poly2_polydata",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "id": null
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [26.9119066141, 37.6536870799],
+                            [26.9493995527, 37.8380664162],
+                            [27.3480965049, 37.7808077919],
+                            [27.306428776, 37.6003127904],
+                            [26.9119066141, 37.6536870799]
+                        ]
+                    ]
+                ]
+            }
+        }]
+    },
+    {
+        "type": "FeatureCollection",
+        "name": "poly3_polydata",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "id": null
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [26.8419532042, 38.1547218821],
+                            [27.253168071, 38.1547218821],
+                            [27.2489576799, 37.6186229349],
+                            [26.8489705228, 37.6208462444],
+                            [26.8419532042, 38.1547218821]
+                        ]
+                    ]
+                ]
+            }
+        }]
+    },
+    {
+        "type": "FeatureCollection",
+        "name": "poly4THISONE",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "id": null
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [26.0062122066, 38.5381072669],
+                            [26.4026198714, 38.5371134534],
+                            [26.4000787966, 38.0274811],
+                            [26.0100238188, 38.024478573],
+                            [26.0062122066, 38.5381072669]
+                        ]
+                    ]
+                ]
+            }
+        }]
+    }
+
+
+
+
+
+
+
+]
