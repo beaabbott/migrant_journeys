2e6b3a7843c62e5e173ec52a33ed8594641a4fe4
bea <92592012+beaabbott@users.noreply.github.com>
2022-03-25 21:57:37 -0400

    finish draft

---
+++ geojson_data/poly_data_v3.js
@@ -0,0 +1,323 @@
+var polygonData3 = [{
+        "type": "FeatureCollection",
+        "name": "poly1",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "name": "poly1",
+                "zoom": 11,
+                "extent": [[37.2343609696999991, 26.9019348692999998], [37.8168020411999990, 27.3309463440000009]] ,
+                "arabic": "هام احذر هذة النقطة هي النقطة كتير بشعة جمعونا بازمير ومشينا بلمكرو تلت ساعات لوصلنا بعد ما وصلنا مشينا حوالي الواحد ونص كيلو متر بلوحل والطين للركب وطبعا نص الطريق شايلين البلم علی كتافنا والنص الباقي عم نسحبو والنسوان قاعدين بقلبو المهم بعد ما وصلنا الشط طلعنا وشدينا مشينا 1:45 دقيقة بلبحر وبعدين كمشونا الخفر التركي يعني المسافة من هي النقطة كانت طويلة اذا شفتو حالكن رح تطلعو من هي النقطة وكان معكن نسوان وأطفال وكبار بلعمر ينصح لاتكملو وربي يفرجها علجميع ",
+                "english": "Important. beware, this point is a very ugly point. They gathered us in Izmir and walked for three hours. After we arrived, we walked about one and a half kilometers in mud and mud up to the knees. Of course, half of the road we were shoveling up to our shoulders, some of us were withdrawing while the women were sitting heavy in their hearts. The important thing is that after we reached the shore, we came out and walked. 1:45 minutes in the sea and then we walked to the Turkish guard. I mean, the distance from this point was long. If you see yourself, you will see where the point is. There are women, children, and adults of age.",
+                "date": "April 2, 2015",
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [26.9019348693, 37.8168020412],
+                            [27.330946344, 37.8168020412],
+                            [27.3291284988, 37.2372555316],
+                            [26.9092062502, 37.2343609697],
+                            [26.9019348693, 37.8124937573],
+                            [26.9019348693, 37.8168020412]
+                        ]
+                    ]
+                ]
+            }
+        }]
+    },
+    // {
+    //     "type": "FeatureCollection",
+    //     "name": "poly2",
+    //     "crs": {
+    //         "type": "name",
+    //         "properties": {
+    //             "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+    //         }
+    //     },
+    //     "features": [{
+    //         "type": "Feature",
+    //         "properties": {
+    //             "name": "poly2",
+    //             "zoom": 9,
+    //             "extent": [[37.6003127903999967, 26.9119066141000012], [37.8380664162000002, 27.3480965048999991]],
+    //             "arabic": " ",
+    //             "english": " ",
+    //             "date": "August 2, 2015",
+    //         },
+    //         "geometry": {
+    //             "type": "MultiPolygon",
+    //             "coordinates": [
+    //                 [
+    //                     [
+    //                         [26.9119066141, 37.6536870799],
+    //                         [26.9493995527, 37.8380664162],
+    //                         [27.3480965049, 37.7808077919],
+    //                         [27.306428776, 37.6003127904],
+    //                         [26.9119066141, 37.6536870799]
+    //                     ]
+    //                 ]
+    //             ]
+    //         }
+    //     }]
+    // },
+    {
+        "type": "FeatureCollection",
+        "name": "poly3",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "name": "poly3",
+                "zoom": 11,
+                "extent": [[37.6186229349263854, 26.8419532042431577], [38.1547218821388228, 27.2531680709875346]],
+                "arabic": "شباب امانة حدا يفيدنا ويتصل لخفر سواحل ليونانية لبلم عم يغرق",
+                "english": "Guys, can anyone help us and call a Greek coast guard to prevent his drowning.",
+                "date": "October 1, 2015",
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [26.8419532042, 38.1547218821],
+                            [27.253168071, 38.1547218821],
+                            [27.2489576799, 37.6186229349],
+                            [26.8489705228, 37.6208462444],
+                            [26.8419532042, 38.1547218821]
+                        ]
+                    ]
+                ]
+            }
+        }]
+    },
+    {
+        "type": "FeatureCollection",
+        "name": "poly4",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "name": "poly4",
+                "zoom": 11,
+                "extent": [[38.0244785729963297, 26.0062122066187129], [38.5381072668871738, 26.4026198714143661]],
+                "arabic": "دعم شباب هاد بلم غرقان بلبحر الكومندوس اخدلهن المحرك احكوا مع الخفر دعم يا شعب دعم",
+                "english": "Help these young people. They are drowning in the sea. The commandos took the engine from them. Talk to guards. Help, people. Help.",
+                "date": "August 11, 2015",
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [26.0062122066, 38.5381072669],
+                            [26.4026198714, 38.5371134534],
+                            [26.4000787966, 38.0274811],
+                            [26.0100238188, 38.024478573],
+                            [26.0062122066, 38.5381072669]
+                        ]
+                    ]
+                ]
+            }
+        }]
+    },
+    {
+        "type": "FeatureCollection",
+        "name": "poly5",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "name": "poly5",
+                "zoom": 12,
+                "extent": [[39.2938300412999979, 26.2372398943999983], [39.5349059006999966, 26.4282004535999988]],
+                "arabic": "دلووا للغنر اليوناني جم عم ينرق",
+                "english": "help, African raft is drowning",
+                "date": "September 11, 2015",
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [26.2372398944, 39.5339767094],
+                            [26.4239836589, 39.5349059007],
+                            [26.4282004536, 39.2938300413],
+                            [26.2402518906, 39.2952286329],
+                            [26.2372398944, 39.5339767094]
+                        ]
+                    ]
+                ]
+            }
+        }]
+    },
+    {
+        "type": "FeatureCollection",
+        "name": "poly6",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "name": "poly6",
+                "zoom": 13,
+                "extent": [[38.2659106054999967, 26.0941344754999989], [38.3771828391000014, 26.3128050125000001]],
+                "arabic": " ",
+                "english": " ",
+                "date": "July 12, 2015",
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [26.0948526053, 38.2659106055],
+                            [26.3128050125, 38.2664744394],
+                            [26.3117278177, 38.3771828391],
+                            [26.0941344755, 38.3769013529],
+                            [26.0948526053, 38.2659106055]
+                        ]
+                    ]
+                ]
+            }
+        }]
+    },
+
+    {
+        "type": "FeatureCollection",
+        "name": "poly7",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "name": "poly7",
+                "zoom": 9,
+                "extent": [[38.4146297998999984, 26.3292646294999990], [39.1499787788999996, 26.8459469113000004]] ,
+                "arabic": "نباب في بغم اتوقف محركو الرجاء انتيليغ بمرعة لئغغر اليوناني او الزكي اذا بعنت بس الظاهر مئفولق ثم١ال",
+                "english": "Young people in raft. Please report rapidly to the Greek or Turkish, I send for you but seems you are busy. ",
+                "date": "September 10, 2015",
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [26.3331788892, 39.1499787789],
+                            [26.8459469113, 39.1499787789],
+                            [26.8400755217, 38.4146297999],
+                            [26.3292646295, 38.416163263],
+                            [26.3331788892, 39.1499787789]
+                        ]
+                    ]
+                ]
+            }
+        }]
+    },
+
+    {
+        "type": "FeatureCollection",
+        "name": "poly8",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "name": "poly8",
+                "zoom": 9,
+                "extent": [[36.6925902832459414, 27.2250193583574251], [37.1443435889090310, 27.5693275649981011]],
+                "arabic": "يا رفاق ، بسرعة",
+                "english": "Guys, quick!",
+                "date": "April 16, 2015",
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [27.2319610561, 37.1443435889],
+                            [27.569327565, 37.143236912],
+                            [27.5637742068, 36.6925902832],
+                            [27.2250193584, 36.6948167393],
+                            [27.2319610561, 37.1443435889]
+                        ]
+                    ]
+                ]
+            }
+        }]
+    },
+
+    {
+        "type": "FeatureCollection",
+        "name": "poly9",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+            }
+        },
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "name": "poly9",
+                "zoom": 9,
+                "extent": [[36.2653784036000033, 27.4953519200999992], [37.1680813609999987, 28.2140131093999997]],
+                "arabic": "جماعه علقانين على انطرف ا لتانبي هن جنرة بعبي بتعنى ",
+                "english": "Please, I am stuck on the other side of the island of Simi, I need help. ",
+                "date": "September 17, 2015",
+            },
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [27.4978215462, 37.1641453334],
+                            [28.2066042312, 37.168081361],
+                            [28.2140131094, 36.271351849],
+                            [27.4953519201, 36.2653784036],
+                            [27.4978215462, 37.1641453334]
+                        ]
+                    ]
+                ]
+            }
+        }]
+    }
+
+
+
+]
